436803f51a5c5090f25d34eab36a2cc427719fb0
Yunfan Li <Rasadell@Yunfans-MacBook-Pro.local>
2016-12-24 12:50:53 +0800

    Count And Say

---
+++ count_and_say.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        def next_str(x):
+            s = str(x)
+            final = []
+            num_c = 0
+            last_c = s[0]
+            for i in xrange(len(s)):
+                if s[i] == last_c:
+                    num_c += 1
+                else:
+                    final.append(str(num_c))
+                    final.append(last_c)
+                    num_c = 1
+                last_c = s[i]
+            final.append(str(num_c))
+            final.append(str(last_c))
+            return ''.join(final)
+        x = '1'
+        for i in xrange(n - 1):
+            x = next_str(x)
+
+        return x
+
+
+s = Solution()
+print s.countAndSay(6)
