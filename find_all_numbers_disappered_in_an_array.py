5cb102eba4e8a62cf6c0ad924fe59407c55bc3da
Yunfan Li <Rasadell@Yunfans-MacBook-Pro.local>
2016-12-27 00:11:53 +0800

    Single Number

--- find_all_numbers_disappered_in_an_array.py
+++ find_all_numbers_disappered_in_an_array.py
@@ -7,10 +7,10 @@ class Solution(object):
         for i in range(len(nums)):
             index = abs(nums[i]) - 1
             nums[index] = - abs(nums[index])
-
+            print nums
         return [i + 1 for i in range(len(nums)) if nums[i] > 0]
 
 
 
 s = Solution()
-print s.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])
+print s.findDisappearedNumbers([4, 6, 5, 7, 6, 5, 3, 1])
---
+++ single_number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in nums:
+            res = res ^ i
+        return res
+
+
+s = Solution()
+print s.singleNumber([2, 2, 1])
