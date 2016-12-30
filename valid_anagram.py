2e6cd259929962b80af075abc745384f4813fc88
Yunfan Li <Rasadell@Yunfans-MacBook-Pro.local>
2016-12-30 13:09:00 +0800

    Valid Anagram

---
+++ valid_anagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
+
+
+s = Solution()
+print s.isAnagram('aa', 'bb')
