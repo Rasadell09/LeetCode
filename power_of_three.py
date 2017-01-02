6903f6f022030df6c707bbc3eb37088c6e2275f1
Yunfan Li <Rasadell@Yunfans-MacBook-Pro.local>
2017-01-02 13:14:42 +0800

    Power of Three

---
+++ power_of_three.py
@@ -0,0 +1,16 @@
+import math
+
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        return (math.log10(n) / math.log10(3)) % 1 == 0
+
+
+s = Solution()
+print s.isPowerOfThree(243)
